af48bde1793c492e063809fbfacf3387b675e942
skippergoroye <oluwatosinadegoroye21@gmail.com>
2024-10-11 15:23:06 +0100

    fix: responsiveness on landing page

--- src/components/landingpage/sections/Hero.tsx
+++ src/components/landingpage/sections/Hero.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React from "react";
 import Image from "next/image";
 import { Button, Container } from "@/components/landingpage";
@@ -6,14 +6,16 @@ import { HeroData } from "@/constants";
 import { useRouter } from "next/navigation";
 
 const Hero = () => {
-  const router = useRouter()
+  const router = useRouter();
   return (
     <Container>
       <div className="lg:w-[800px] 2xl:w-[80em] mx-auto flex flex-col text-center items-center gap-[1em]">
-        <h1 className="font-rubik font-[800] text-[5em] leading-[1em] text-sec-901 mb-4">
-          Hire <span className="text-main-500">Top Talents</span>
-          &nbsp; <br /> 10x faster with Vamp.
+ 
+        <h1 className="font-rubik font-[800] text-[2em] md:text-[5em] leading-[1.2em] md:leading-[1em] text-sec-901 mb-4">
+          Hire <span className="text-main-500">Top Talents</span> <br />
+          10x faster with Vamp.
         </h1>
+
         <p className="md:w-[70%] 2xl:w-[50%]  font-jakarta text-sec-901 font-[300] leading-[1.2em] text-[1.25em]">
           Simplify your hiring process, reduce time-to-hire, and make confident
           decisions with access to a diverse pool of qualified candidates
@@ -22,7 +24,7 @@ const Hero = () => {
         <Button
           text="Get started"
           variant="bg-main-600 text-white rounded-lg w-[250px] h-[5em] mt-[2.5em]"
-          clickFn={()=> router.push('/waitlist')}
+          clickFn={() => router.push("/waitlist")}
         />
       </div>
 
