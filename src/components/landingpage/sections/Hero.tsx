8f91c61d9ba349c63a9068b6583fc8a90dbc2b25
skippergoroye <oluwatosinadegoroye21@gmail.com>
2024-10-17 16:53:18 +0100

    fix: landing page and waitlist fixes

--- src/components/landingpage/sections/Hero.tsx
+++ src/components/landingpage/sections/Hero.tsx
@@ -10,7 +10,6 @@ const Hero = () => {
   return (
     <Container>
       <div className="lg:w-[800px] 2xl:w-[80em] mx-auto flex flex-col text-center items-center gap-[1em]">
- 
         <h1 className="font-rubik font-[800] text-[2em] md:text-[5em] leading-[1.2em] md:leading-[1em] text-sec-901 mb-4">
           Hire <span className="text-main-700">Top Talents</span> <br />
           10x faster with Vamp.
@@ -21,7 +20,6 @@ const Hero = () => {
           decisions with access to a diverse pool of qualified candidates
         </p>
 
-
         <Button
           text="Get started"
           variant="bg-main-700 text-white rounded-lg w-[250px] h-[5em] mt-[2.5em]"
@@ -29,15 +27,30 @@ const Hero = () => {
         />
       </div>
 
-      <div className="mt-[5em] grid  place-items-center grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 items-center justify-center gap-5">
-        {HeroData.map((card, index) => (
-          <div key={index} className="mt-2">
-            <div className="border-t-2 border-t-main-100 mb-4">
-              {card.title}
+      <div className="mt-[5em]">
+        {/* Mobile view: Flexbox with horizontal scrolling */}
+        <div className="flex sm:hidden overflow-x-scroll w-full space-x-5">
+          {HeroData.map((card, index) => (
+            <div key={index} className="mt-2 min-w-[80%] sm:min-w-0">
+              <div className="border-t-2 border-t-main-100 mb-4">
+                {card.title}
+              </div>
+              <Image src={card.imgURL} alt="hero-image" />
             </div>
-            <Image src={card.imgURL} alt="hero-image" />
-          </div>
-        ))}
+          ))}
+        </div>
+
+        {/* Desktop view: Grid layout */}
+        <div className="hidden sm:grid place-items-center grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 items-center justify-center gap-5">
+          {HeroData.map((card, index) => (
+            <div key={index} className="mt-2">
+              <div className="border-t-2 border-t-main-100 mb-4">
+                {card.title}
+              </div>
+              <Image src={card.imgURL} alt="hero-image" />
+            </div>
+          ))}
+        </div>
       </div>
     </Container>
   );
