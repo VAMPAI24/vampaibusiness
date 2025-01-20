62d397f3830cf3993c13610afc4c2f02bf0672c0
aphilemon.aa <aphilemon.aa@gmail.com>
2025-01-20 23:30:22 +0100

    made changes to ai comment

--- src/redux/features/api/apiSlice.ts
+++ src/redux/features/api/apiSlice.ts
@@ -1,12 +1,13 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { BASE_URL } from "../constants";
+import { BASE_URL, PROD_BASE_URL } from "../constants";
 import { RootState } from "@/redux/app/store";
-
+import { isLocalOrStaging } from "@/lib/utils";
 
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: BASE_URL,
+    // baseUrl: BASE_URL,
+    baseUrl: isLocalOrStaging() ? BASE_URL : PROD_BASE_URL,
     prepareHeaders: (headers, { getState }) => {
       // Access the token from the Redux state, assuming it's stored in `token`
       const token = (getState() as RootState).auth?.token;
@@ -26,4 +27,3 @@ export const apiSlice = createApi({
   tagTypes: ["Users"],
   endpoints: () => ({}),
 });
-
